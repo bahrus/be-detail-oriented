c73401a49ffe6baf7190eccfdcbbf4ce4a14cd60
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-11-24 20:41:01 -0500

    0.0.3

--- be-detail-oriented.ts
+++ be-detail-oriented.ts
@@ -94,13 +94,13 @@ define<Proxy & BeDecoratedProps<Proxy, Actions>, Actions>({
 >
     <template shadowroot=open>
     <form>
-        <button type=button name=expand part=expand aria-label=Expand>
+        <button type=button name=expand part="expand button" aria-label=Expand>
             <svg width="16px" height="16px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-plus-square">
                 <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
             </svg>
         </button>
-        <button type=button name=collapse part=collapse aria-label=Collapse hidden>
+        <button type=button name=collapse part="collapse button" aria-label=Collapse hidden>
             <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve">
                 <g>
                     <line fill="none" stroke="#000000" stroke-width="4" stroke-miterlimit="10" x1="14" y1="31" x2="50" y2="31"/>
@@ -112,6 +112,22 @@ define<Proxy & BeDecoratedProps<Proxy, Actions>, Actions>({
     <style adopt>
         form {
             display: inline;
+        }
+        button {
+            background-color: transparent;
+            background-repeat: no-repeat;
+            border: none;
+            cursor: pointer;
+            overflow: hidden;
+            outline: none;
+            margin: 0;
+            padding: 0;
+            position: relative;
+            top: 2px;
+        }
+        :host{
+            margin: 0;
+            padding: 0;
         }
     </style>
    </template>
